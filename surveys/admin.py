7af51bfd4eda16c0ff94797ccef0953bb7e31196
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-08-20 09:36:59 +0300

    Surveys model and admin panel

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from nested_inline.admin import NestedTabularInline, NestedStackedInline, NestedModelAdmin
+from surveys.models import Survey, Question, Option
+
+class OptionInLine(NestedTabularInline):
+    model = Option
+    extra = 1
+    fields = ["value", "correct"]
+
+class QuestionInLine(NestedStackedInline):
+    model = Question
+    extra = 1
+    inlines = [OptionInLine]
+
+class SurveyAdmin(NestedModelAdmin):
+    inlines = [QuestionInLine]
 
 # Register your models here.
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Question)
+admin.site.register(Option)
