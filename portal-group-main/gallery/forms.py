ab2cbee60e09f52f120a55c7d523be4d75cb3b13
Shatalova Tonya <tonechka.shatalova@gmail.com>
2024-08-23 14:17:08 +0300

    Create models and forms

---
+++ portal-group-main/gallery/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Media
+
+class MediaForm(forms.ModelForm):
+    class Meta:
+        model = Media
+        fields = ['title', 'media_type', 'file']
--- portal-group-main/gallery/models.py
+++ portal-group-main/gallery/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Media(models.Model):
+    MEDIA_TYPE_CHOICES = (
+        ('photo', 'Фото'),
+        ('video', 'Відео'),
+    )
+
+    title = models.CharField(max_length=100)
+    media_type = models.CharField(max_length=5, choices=MEDIA_TYPE_CHOICES)
+    file = models.FileField(upload_to='media/')
+    data = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
